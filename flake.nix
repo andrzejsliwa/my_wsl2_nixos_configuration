8d78c26a24d0a6d478765196ca10d451d3fde28e
andrzejsliwa <andrzej.sliwa@gmail.com>
2024-10-23 15:38:53 +0200

    cleanup in nix config

--- flake.nix
+++ flake.nix
@@ -1,43 +1,49 @@
 {
   description = "NixOS configuration";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-  inputs.nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  inputs.home-manager.url = "github:nix-community/home-manager/release-24.05";
-  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-  inputs.nur.url = "github:nix-community/NUR";
-  inputs.nixos-wsl.url = "github:nix-community/NixOS-WSL";
-  inputs.nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+    nur.url = "github:nix-community/NUR";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL";
+    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
 
-  inputs.nix-index-database.url = "github:Mic92/nix-index-database";
-  inputs.nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+    nix-index-database.url = "github:Mic92/nix-index-database";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+
+    alejandra.url = "github:kamadorueda/alejandra/3.0.0";
+    alejandra.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
   outputs = inputs:
-    with inputs; let
+    with inputs;
+    let
       secrets = builtins.fromJSON (builtins.readFile "${self}/secrets.json");
 
-      nixpkgsWithOverlays = system: (import nixpkgs rec {
-        inherit system;
+      nixpkgsWithOverlays = system:
+        (import nixpkgs rec {
+          inherit system;
 
-        config = {
-          allowUnfree = true;
-          permittedInsecurePackages = [
+          config = {
+            allowUnfree = true;
+            permittedInsecurePackages = [ ];
+          };
+
+          overlays = [
+            nur.overlay
+
+            (_final: prev: {
+              unstable = import nixpkgs-unstable {
+                inherit (prev) system;
+                inherit config;
+              };
+            })
           ];
-        };
-
-        overlays = [
-          nur.overlay
-
-          (_final: prev: {
-            unstable = import nixpkgs-unstable {
-              inherit (prev) system;
-              inherit config;
-            };
-          })
-        ];
-      });
+        });
 
       configurationDefaults = args: {
         home-manager.useGlobalPkgs = true;
@@ -48,29 +54,19 @@
 
       argDefaults = {
         inherit secrets inputs self nix-index-database;
-        channels = {
-          inherit nixpkgs nixpkgs-unstable;
-        };
+        channels = { inherit nixpkgs nixpkgs-unstable; };
       };
 
-      mkNixosConfiguration = {
-        system ? "x86_64-linux",
-        hostname,
-        username,
-        args ? {},
-        modules,
-      }: let
-        specialArgs = argDefaults // {inherit hostname username;} // args;
-      in
-        nixpkgs.lib.nixosSystem {
+      mkNixosConfiguration =
+        { system ? "x86_64-linux", hostname, username, args ? { }, modules, }:
+        let specialArgs = argDefaults // { inherit hostname username; } // args;
+        in nixpkgs.lib.nixosSystem {
           inherit system specialArgs;
           pkgs = nixpkgsWithOverlays system;
-          modules =
-            [
-              (configurationDefaults specialArgs)
-              home-manager.nixosModules.home-manager
-            ]
-            ++ modules;
+          modules = [
+            (configurationDefaults specialArgs)
+            home-manager.nixosModules.home-manager
+          ] ++ modules;
         };
     in {
       formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
@@ -78,10 +74,7 @@
       nixosConfigurations.nixos = mkNixosConfiguration {
         hostname = "nixos";
         username = "andrzejsliwa"; # FIXME: replace with your own username!
-        modules = [
-          nixos-wsl.nixosModules.wsl
-          ./wsl.nix
-        ];
+        modules = [ nixos-wsl.nixosModules.wsl ./wsl.nix ];
       };
     };
 }
