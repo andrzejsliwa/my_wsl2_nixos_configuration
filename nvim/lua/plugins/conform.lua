a7bcda2965652770a02bd5b0bac6d087ef18b6bd
andrzejsliwa <andrzej.sliwa@gmail.com>
2024-11-15 14:48:32 +0100

    vim refresh

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,6 +4,13 @@ return {
   opts = {
     formatters_by_ft = {
       nix = { "alejandra" },
+      eruby = { "htmlbeautifier" },
+      ruby = { "standardrb" },
+      markdown = { "markdownlint" },
+      yaml = { "yamlfix" },
+      javascript = { "prettierd", "prettier" },
+      typescript = { "prettierd", "prettier" },
+      json = { "jq" },
     },
   },
 }
